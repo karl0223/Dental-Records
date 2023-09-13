43c508cd324c94506a82d887fc9ae8b301e7410d
karl ducta <karlducta022@gmail.com>
2023-09-13 14:29:18 +0800

    added debug tool bar

--- dentalclinic/urls.py
+++ dentalclinic/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar #Import debug toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')) # /playground/
+    path('playground/', include('playground.urls')), # /playground/
+    path('__debug__/', include(debug_toolbar.urls))
 ]
