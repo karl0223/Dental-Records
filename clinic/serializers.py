427ac4f07344d369ea6cd0d3f9fe6c2f393960ca
karl ducta <karlducta022@gmail.com>
2023-09-20 13:22:21 +0800

    Model Serializer -- Better way to implement Serializers

--- clinic/serializers.py
+++ clinic/serializers.py
@@ -1,11 +1,18 @@
 from decimal import Decimal
 from rest_framework import serializers
 
-class PackageSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    package_type = serializers.CharField(max_length=1)
-    package_price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price')
+from clinic.models import Package
+
+class PackageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Package
+        fields = ['title', 'package_type', 'price', 'price_with_discount']
+
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # package_type = serializers.CharField(max_length=1)
+    # package_price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price')
     price_with_discount = serializers.SerializerMethodField(method_name='calculate_discount')
 
     def calculate_discount(self, package):
