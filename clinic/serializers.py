00816cb7d92b6353c4d2b7d0f772565bcc5c0418
karl ducta <karlducta022@gmail.com>
2023-09-20 11:34:46 +0800

    Creating Custom Serializer fields and renaming fields

--- clinic/serializers.py
+++ clinic/serializers.py
@@ -1,7 +1,12 @@
+from decimal import Decimal
 from rest_framework import serializers
 
 class PackageSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     package_type = serializers.CharField(max_length=1)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    package_price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price')
+    price_with_discount = serializers.SerializerMethodField(method_name='calculate_discount')
+
+    def calculate_discount(self, package):
+        return package.price - (package.price * Decimal(.10))
