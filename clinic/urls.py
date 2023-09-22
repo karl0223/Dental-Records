d07ad7b44fe956c9c04c91e140d10ac26e46f279
karl ducta <karlducta022@gmail.com>
2023-09-22 21:58:35 +0800

    --- Class Based API Views --- Converting function based api views to class based ---

--- clinic/urls.py
+++ clinic/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     #/playground/hello/ - url
-    path('package/', views.package_list), # always add '/' at the end
-    path('package/<int:id>/', views.package_details), # always add '/' at the end
-    path('patient/', views.patient_list),
-    path('patient/<int:id>/', views.patient_details),
+    path('package/', views.PackageList.as_view()), # always add '/' at the end
+    path('package/<int:id>/', views.PackageDetail.as_view()), # always add '/' at the end
+    path('patient/', views.PatientList.as_view()),
+    path('patient/<int:id>/', views.PatientDetail.as_view()),
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -1,47 +1,68 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from clinic.models import Package, Patient
 from .serializers import PackageSerializer, PatientSerializer
 
-# Create your views here.
-@api_view(['GET', 'POST'])
-def package_list(request):
-    if request.method == "GET":
+
+class PackageList(APIView):
+    def get(self, request):
         queryset = Package.objects.all()
         serializer = PackageSerializer(queryset, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    
+    def post(self, request):
         serializer = PackageSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        serializer.validated_data
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED) 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def package_details(request, id):
-    package = get_object_or_404(Package, pk=id)
-    if request.method == 'GET':
+class PackageDetail(APIView):
+    def get(self, request, id):
+        package = get_object_or_404(Package, pk=id)
         serializer = PackageSerializer(package)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    
+    def put(self, request, id):
+        package = get_object_or_404(Package, pk=id)
         serializer = PackageSerializer(package, data=request.data) # Item - data
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request, id):
+        package = get_object_or_404(Package, pk=id)
         package.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+       
+class PatientList(APIView):
+    def get(self, request):
+        queryset = Patient.objects.all()
+        serializer = PatientSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'POST'])
-def patient_list(request):
-    queryset = Patient.objects.all()
-    serializer = PatientSerializer(queryset, many=True)
-    return Response(serializer.data)
+class PatientDetail(APIView):
+    def get(self, request, id):
+        patient = get_object_or_404(Patient, pk=id)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        patient = get_object_or_404(Patient, pk=id)
+        serializer = PatientSerializer(patient, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view()
-def patient_details(request, id):
-    patient = get_object_or_404(Patient, pk=id)
-    serializer = PatientSerializer(patient)
-    return Response(serializer.data)
+    def delete(self, request, id):
+        patient = get_object_or_404(Patient, pk=id)
+        patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
