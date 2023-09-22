508717e470cf6e32d1a6498016dc1dd196da5335
karl ducta <karlducta022@gmail.com>
2023-09-22 22:21:45 +0800

    --- Model Mixins --- Improved class based views --- new implementation ---

--- clinic/views.py
+++ clinic/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework import status
 from clinic.models import Package, Patient
 from .serializers import PackageSerializer, PatientSerializer
