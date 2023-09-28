f554fc1ed243b9bedb6b0a32d9cf66c2da3247f6
karl ducta <karlducta022@gmail.com>
2023-09-28 22:20:13 +0800

    ------- EXTENDING USER MODEL -------- CUSTOMIZE USERADMIN FIELDS IN ADMIN PANEL -------

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-class User(AbstractBaseUser):
-    pass
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
