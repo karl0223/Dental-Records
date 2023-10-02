f5078702190ecd33e8e581d95211b63e0be5cd40
karl ducta <karlducta022@gmail.com>
2023-10-02 11:34:14 +0800

    --- GETTING THE CURRENT USER --- MODHEADER --- ADD FIELDS IN THE USER ENDPOINT USING SERIALIZERS ---

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,10 @@
-from djoser.serializers import UserCreateSerializer as BaseCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseCreateSerializer
 
 class UserCreateSerializer(BaseCreateSerializer):
     class Meta(BaseCreateSerializer.Meta):              # Inherit the Meta class of BaseCreateSerializer
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
