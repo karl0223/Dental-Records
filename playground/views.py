a9a4b1a04fec2aa211aa1adff49b74020f549986
karl ducta <karlducta022@gmail.com>
2023-10-08 01:26:33 +0800

    --- ATTACH FILES IN EMAILS --- CONTROL OVER THE EMAILS SENT ---

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, EmailMessage ,BadHeaderError
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,7 +6,12 @@ from django.shortcuts import render
 def say_hello(request):
     try:
         # send_mail('subject', 'message', 'email', ['list of recipients'])
-        mail_admins('subject', 'message', html_message='message')
+        # mail_admins('subject', 'message', html_message='message')
+
+        message = EmailMessage('subject', 'message', 'from@karl.com', ['from@bob.com'])
+        message.attach_file('playground/static/images/dog.jpg')
+        message.send()
+
     except BadHeaderError:
         pass
     return render(request, 'hello.html', { 'name' : 'Karl'})
