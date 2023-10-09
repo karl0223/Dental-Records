c7ed7c715092227713fa201dbc96d67313b8d50f
karl ducta <karlducta022@gmail.com>
2023-10-09 16:01:29 +0800

    --- RUN TEST CASES --- TEST SPECIFIC DIRECTORY/FILE/CLASSES/FUNCTIONS ---

--- clinic/tests/test_packages.py
+++ clinic/tests/test_packages.py
@@ -1,6 +1,8 @@
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
+@pytest.mark.django_db
 class TestCreatePackage:
     def test_if_user_is_anonymous_returns_401(self):
         client = APIClient()
